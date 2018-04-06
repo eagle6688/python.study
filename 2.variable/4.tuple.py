831a4e633162e0a3ee5e05b87f44865076752918
Aldwin Su <fangyansu@163.com>
2018-04-06 19:31:50 +0800

    Update

--- 2.variable/4.tuple.py
+++ 2.variable/4.tuple.py
@@ -3,13 +3,20 @@ tuple = ('Hello', 786, 2.23, 'john', 70.2)
 tinytuple = (123, 'john')
 
 # Sub tuple
+print('\n')
+print('Sub tuple'.center(50, '*'))
+
 print(tuple)
 print(tuple[0])
 print(tuple[1:3])
 print(tuple[2:])
 
 # Repeat
+print('\n')
+print('Repeat'.center(50, '*'))
 print(tinytuple * 2)
 
 # Concat
+print('\n')
+print('Concat'.center(50, '*'))
 print(tuple + tinytuple)
--- 2.variable/5.dictionary.py
+++ 2.variable/5.dictionary.py
@@ -1,10 +1,123 @@
+# Dictionary
+print('\n')
+print('Dictionary'.center(50, '*'))
+
 dict = {}
 dict['one'] = 'This is one'
 dict[2] = 'This is two'
 print(dict)
 
-tinydict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
-print(tinydict)
-print(tinydict.keys())
-print(tinydict.values())
-print(tinydict.items())
+# Change
+print('\n')
+print('Change'.center(50, '*'))
+
+dict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+print(dict)
+
+# keys
+print('\n')
+print('keys'.center(50, '*'))
+print(dict.keys())
+
+# values
+print('\n')
+print('values'.center(50, '*'))
+print(dict.values())
+
+# items
+print('\n')
+print('items'.center(50, '*'))
+print(dict.items())
+
+# del
+print('\n')
+print('del'.center(50, '*'))
+
+del dict['dept']
+print(dict)
+
+# clear
+print('\n')
+print('clear'.center(50, '*'))
+
+dict.clear()
+print(dict)
+
+# str
+print('\n')
+print('str'.center(50, '*'))
+
+dict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+print(str(dict))
+
+# type
+print('\n')
+print('type'.center(50, '*'))
+print(type(dict))
+
+# copy
+print('\n')
+print('copy'.center(50, '*'))
+
+dict1 = {'user': 'runoob', 'num': [1, 2, 3]}
+dict2 = dict1  # Refer object
+dict3 = dict1.copy()  # Shallow copy
+
+dict1['user'] = 'root'
+dict1['num'].pop()
+
+print(dict1)
+print(dict2)
+print(dict3)
+
+# fromkeys
+print('\n')
+print('fromkeys'.center(50, '*'))
+
+seq = ('name', 'age', 'sex')
+dict = dict.fromkeys(seq)
+print(dict)
+
+dict = dict.fromkeys(seq, 10)
+print(dict)
+
+# get
+print('\n')
+print('get'.center(50, '*'))
+
+dict = {'Name': 'Zara', 'Age': 27}
+print(dict.get('Name'))
+print(dict.get('Sex', 'Never'))
+
+# __contains__
+print('\n')
+print('__contains__'.center(50, '*'))
+print(dict.__contains__('Age'))
+print(dict.__contains__('Value'))
+
+# setdefault
+print('\n')
+print('setdefault'.center(50, '*'))
+
+value = dict.setdefault('ecoom', 'Taobao')
+print(value)
+print(dict)
+
+# update
+print('\n')
+print('update'.center(50, '*'))
+
+dict1 = {'Name': 'Zara', 'Age': 7}
+dict2 = {'Sex': 'female'}
+dict1.update(dict2)
+print(dict1)
+
+# pop
+print('\n')
+print('pop'.center(50, '*'))
+
+value = dict1.pop('Sex')
+print(value)
+
+value = dict1.pop('Sex', 'male')
+print(value)
