06fa07b664768ccac53c31a5c1a469790dbbec0c
Aldwin Su <fangyansu@163.com>
2018-04-06 18:19:27 +0800

    Update

--- 2.variable/4.tuple.py
+++ 2.variable/4.tuple.py
@@ -1,10 +1,15 @@
+# tuple
 tuple = ('Hello', 786, 2.23, 'john', 70.2)
 tinytuple = (123, 'john')
 
+# Sub tuple
 print(tuple)
 print(tuple[0])
 print(tuple[1:3])
 print(tuple[2:])
 
+# Repeat
 print(tinytuple * 2)
+
+# Concat
 print(tuple + tinytuple)
